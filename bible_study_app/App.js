d7529e2700c27dd241945d09964580a75f21d8e2
Anastacia Castro <ac2@wellesley.edu>
2021-01-06 20:39:09 -0600

    Adding an image navigation bar. Still to work on: adding padding between images
    - Anastacia

--- bible_study_app/App.js
+++ bible_study_app/App.js
@@ -1,7 +1,7 @@
 import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { Button, StyleSheet, Text, View, TextInput } from 'react-native';
+import { Alert, Button, StyleSheet, Text, View, TextInput, Image, TouchableOpacity } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
@@ -44,9 +44,46 @@ export default function App() {
 function MainHeader() {
   return (
     <View style = {styles.header}>
+    <TouchableOpacity
+      onPress = {() => Alert.alert("menu pressed")}
+    >
     <Image
-      source = {require('
+      style = { styles.tinyLogo }
+      source = {{
+        uri: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn.designcrowd.com%2Fblog%2F2016%2FMarch%2Ficons-vs-logos%2FHamburger-Icon_300.png&f=1&nofb=1'
+      }}
     />
+    </TouchableOpacity>
+    <TouchableOpacity
+      onPress = {() => Alert.alert("home pressed")}
+      >
+      <Image
+      style = { styles.tinyLogo }
+      source = {{
+        uri: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fthumb%2F2%2F26%2FIos-home-outline.svg%2F768px-Ios-home-outline.svg.png&f=1&nofb=1'
+      }}
+    />
+    </TouchableOpacity>
+    <TouchableOpacity
+      onPress = {() => Alert.alert("chat pressed")}
+      >
+      <Image
+      style = { styles.tinyLogo }
+      source = {{
+        uri: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn.onlinewebfonts.com%2Fsvg%2Fimg_503325.png&f=1&nofb=1' 
+      }}
+    />
+    </TouchableOpacity>
+    <TouchableOpacity
+      onPress = {() => Alert.alert("prayer pressed")}
+      >
+      <Image
+      style = { styles.tinyLogo }
+      source = {{
+        uri: 'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.clipartbest.com%2Fcliparts%2FaTq%2Fb4g%2FaTqb4gLTM.gif&f=1&nofb=1' 
+      }}
+    />
+    </TouchableOpacity>
     </View>
   );
 }
@@ -90,5 +127,14 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  tinyLogo: {
+    width: 40,
+    height: 40,
+  },
+  header: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
   },
 });
