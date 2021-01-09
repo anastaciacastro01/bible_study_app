647b8a10699c5deffb10de949a49f2eff71e7c2b
Anastacia Castro <ac2@wellesley.edu>
2021-01-09 12:14:34 -0600

    Should be added with last commit
    - anastacia

---
+++ bible_study_app/screens/StudiesScreen.js
@@ -0,0 +1,11 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export const StudiesScreen = ({ navigation, route }) => {
+  return (
+    <View>
+      <Text> This is the studies screen </Text>
+    </View>
+  );
+}
