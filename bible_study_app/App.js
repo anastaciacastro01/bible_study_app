a7c6b90511fe378a031878c2f8d4b287bd4926c5
Anastacia Castro <ac2@wellesley.edu>
2021-01-09 12:13:57 -0600

    Started building infrastructure for adding screens
    - anastacia

--- bible_study_app/App.js
+++ bible_study_app/App.js
@@ -6,6 +6,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { Ionicons } from '@expo/vector-icons';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { StudiesScreen } from './screens/StudiesScreen.js';
 
 /**
  * navigation
@@ -13,7 +14,9 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 const Stack = createStackNavigator(); // currently using tab navigation instead
 const Tab = createBottomTabNavigator();
 
-// const MyStack = () => {
+/**
+ * main structure for navigation
+ */
 export default function App() {
   return (
     <NavigationContainer>
@@ -73,61 +76,6 @@ export default function App() {
     </NavigationContainer>
   )
 }
-
-function MainHeader() {
-  return (
-    <View style = {styles.header}>
-    <TouchableOpacity
-      onPress = {() => Alert.alert("menu pressed")}
-    >
-    <Image
-      style = { styles.tinyLogo }
-      source = {{
-        uri: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn.designcrowd.com%2Fblog%2F2016%2FMarch%2Ficons-vs-logos%2FHamburger-Icon_300.png&f=1&nofb=1'
-      }}
-    />
-    </TouchableOpacity>
-    <TouchableOpacity
-      onPress = {() => Alert.alert("home pressed")}
-      >
-      <Image
-      style = { styles.tinyLogo }
-      source = {{
-        uri: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fthumb%2F2%2F26%2FIos-home-outline.svg%2F768px-Ios-home-outline.svg.png&f=1&nofb=1'
-      }}
-    />
-    </TouchableOpacity>
-    <TouchableOpacity
-      onPress = {() => Alert.alert("chat pressed")}
-      >
-      <Image
-      style = { styles.tinyLogo }
-      source = {{
-        uri: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn.onlinewebfonts.com%2Fsvg%2Fimg_503325.png&f=1&nofb=1' 
-      }}
-    />
-    </TouchableOpacity>
-    <TouchableOpacity
-      onPress = {() => Alert.alert("prayer pressed")}
-      >
-      <Image
-      style = { styles.tinyLogo }
-      source = {{
-        uri: 'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.clipartbest.com%2Fcliparts%2FaTq%2Fb4g%2FaTqb4gLTM.gif&f=1&nofb=1' 
-      }}
-    />
-    </TouchableOpacity>
-    </View>
-  );
-}
-
-const StudiesScreen = ({ navigation, route }) => {
-  return (
-    <View style = { styles.generalView } >
-    </View>
-  );
-};
-
 const ProfileScreen = ({ navigation, route }) => {
   return (
     <View style = { styles.generalView } >
@@ -155,17 +103,5 @@ const styles = StyleSheet.create({
   },
   generalView: {
     flex: 1,
-  },
-  tinyLogo: {
-    flex: 1,
-    width: 40,
-    height: 40,
-  },
-  header: {
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    backgroundColor: 'blue',
-    flexGrow: 1,
   },
 });
