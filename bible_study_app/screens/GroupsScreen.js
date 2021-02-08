c5f47c11fa14e29951dabf3753100220a0824043
Anastacia Castro <ac2@wellesley.edu>
2021-02-08 14:11:45 -0600

    Added more functionality to the Groups screen - now you can click into any group and see buttons for a chat, studies, and a prayer board. Might want to rethink the design of the page. Also, still need to add more group functionalities such as an 'about', list of members, invite people to join, etc. Also need to develop the interface for a group that the user is not a member of. In addition, I need to figure out what a public vs. private group page will look like.
    - anastacia

--- bible_study_app/screens/GroupsScreen.js
+++ bible_study_app/screens/GroupsScreen.js
@@ -28,6 +28,7 @@ const FONT_SIZES = {
   pageHeader: 30,
   iconSize: 25,
   groupTitle: 18,
+  buttonTitle: 18,
 }
 
 const GROUPS = [
@@ -35,49 +36,57 @@ const GROUPS = [
       pic: 'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwallpapercave.com%2Fwp%2FWVN2cWE.jpg&f=1&nofb=1',
       title: "Group 1",
       isMember: true,
-      isPrivate: false,
+      isPrivate: true,
+      groupId: 1,
     },
     {
       pic: 'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwallpapercave.com%2Fwp%2FWVN2cWE.jpg&f=1&nofb=1',
       title: "Group 2",
       isMember: false,
       isPrivate: false,
+      groupId: 2, 
     },
     {
       pic: 'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwallpapercave.com%2Fwp%2FWVN2cWE.jpg&f=1&nofb=1',
       title: "Group 3",
       isMember: true,
       isPrivate: false,
+      groupId: 3, 
     },
     {
       pic: 'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwallpapercave.com%2Fwp%2FWVN2cWE.jpg&f=1&nofb=1',
       title: "Group 4",
       isMember: true,
       isPrivate: false,
+      groupId: 4, 
     },
     {
       pic: 'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwallpapercave.com%2Fwp%2FWVN2cWE.jpg&f=1&nofb=1',
       title: "Group 5",
       isMember: false,
       isPrivate: false,
+      groupId: 5, 
     },
     {
       pic: 'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwallpapercave.com%2Fwp%2FWVN2cWE.jpg&f=1&nofb=1',
       title: "Group 6",
       isMember: false,
       isPrivate: false,
+      groupId: 6, 
     },
     {
       pic: 'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwallpapercave.com%2Fwp%2FWVN2cWE.jpg&f=1&nofb=1',
       title: "Group 7",
       isMember: false,
       isPrivate: false,
+      groupId: 7, 
     },
     {
       pic: 'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwallpapercave.com%2Fwp%2FWVN2cWE.jpg&f=1&nofb=1',
       title: "Group 8",
       isMember: true,
       isPrivate: false,
+      groupId: 8, 
     },
 ];
 
@@ -90,22 +99,31 @@ const GROUPS = [
  *          blurb - short blurb about the study
  *          verses - verses that the study will go over (optional)
  */
-const Group = ({ pic, nav, title, isMember, isPrivate, }) => {
+const Group = ({ pic, nav, title, isMember, isPrivate, groupId, }) => {
   const thisWindow = useWindowDimensions();
 
+  //      <Image source = { pic } style = { styles.image }/>
   return (
     <View>
       <Pressable 
         style = {{ 
           flex: 1, 
-          width: thisWindow.width / 3, 
-          height: thisWindow.width / 2, 
+          minWidth: thisWindow.width / 3, 
+          minHeight: thisWindow.width / 2, 
           marginHorizontal: 10,
           borderRadius: 10,
           overflow: 'hidden',
+          backgroundColor: 'seagreen',
+          justifyContent: 'flex-end',
+        }}
+        onPress = {() => {
+          nav.navigate("Group", {
+            groupId: groupId,
+            isMember: isMember,
+            name: title,
+          });
         }}
       >
-        <Image source = { pic } style = { styles.image }/>
         <Text style = { styles.caption }> { title } </Text>
       </Pressable>
     </View>
@@ -121,7 +139,7 @@ const renderGroupList = ( contents, navigation ) => (
           nav = { navigation }
           title = { group.title }
           isMember = { group.isMember }
-          isPrivate = { group.isPrivate }
+          groupId = { group.groupId }
         />
       );
     }
@@ -131,8 +149,36 @@ const renderGroupList = ( contents, navigation ) => (
 const GroupsScreen = ({ navigation, route }) => {
   let bool = true;
 
+  React.useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+            <View style = { styles.pageHeader }>
+              <Pressable
+                onPress = {() => Alert.alert('Search pressed')}
+                style = { styles.tinyIcon }
+              >
+                <Ionicons name = "ios-search-outline" size = { FONT_SIZES.iconSize } color = { "mediumseagreen" }/>
+              </Pressable>
+              <Pressable
+                onPress = {() => Alert.alert('Join Group Pressed')} 
+                style = { styles.tinyIcon }
+              >
+                <Ionicons name = "ios-person-add-outline" size = { FONT_SIZES.iconSize } color = { "mediumseagreen" }/>
+              </Pressable>
+              <Pressable
+                onPress = {() => Alert.alert('Create Group Pressed')} 
+                style = { styles.tinyIcon }
+              >
+                <Ionicons name = "ios-add-circle-outline" size = { FONT_SIZES.iconSize } color = { "mediumseagreen" }/>
+              </Pressable>
+            </View>
+      ),
+    });
+  }, [navigation]);
+
   return (
     <SafeAreaView style = { styles.container }>
+    <ScrollView>
       <Section
         title = "My Groups"
         contents = { GROUPS.filter((group) => group.isMember) }
@@ -140,32 +186,124 @@ const GroupsScreen = ({ navigation, route }) => {
         renderFunc = { renderGroupList }
       />
       <Section
-        title = "Browse Groups"
+        title = "Recommended Groups"
         contents = { GROUPS.filter((group) => !group.isMember) }
         navigation = { navigation }
         renderFunc = { renderGroupList }
       />
+      <Section
+        title = "Popular Groups"
+        contents = { GROUPS.filter((group) => group.isMember) }
+        navigation = { navigation }
+        renderFunc = { renderGroupList }
+      />
+      </ScrollView>
     </SafeAreaView>
   );
 };
 
-const MemberGroupScreen = () => {
+const MemberGroupScreen = ({ groupId }) => {
+  const myGroup = GROUPS.filter((group) => group.groupId == groupId)[0];
+  const thisWindow = useWindowDimensions();
+
   return(
-    <Text> I am a member of this group </Text>
+    <SafeAreaView style = { styles.container } >
+      <ScrollView>
+        <Image source = {{ uri: myGroup.pic }} 
+               style = {{  
+                flex: 1,
+                resizeMode: 'cover',
+                justifyContent: 'flex-end',
+                alignItems: 'center',
+                height: thisWindow.height / 3,
+               }}
+        />
+        <View style = { styles.section }>
+          <View style = {{ flexDirection: 'row', marginHorizontal: 5, marginTop: 10, }}>
+            <Pressable
+            style = {{
+          flex: 1, 
+          height: thisWindow.width / 2, 
+          marginHorizontal: 5,
+          borderRadius: 10,
+          overflow: 'hidden',
+          backgroundColor: 'mediumseagreen',
+          justifyContent: 'space-evenly',
+          alignItems: 'center',
+          }}
+            >
+              <Ionicons name = "ios-chatbubbles-outline" size = { 40 } color = { "white" }/>
+              <Text style = {{
+                              fontSize: FONT_SIZES.buttonTitle,
+                              color: "white",
+                              padding: 5,
+                            }}
+              >
+                Chat
+              </Text>
+            </Pressable>
+            <Pressable
+            style = {{
+          flex: 1, 
+          height: thisWindow.width / 2, 
+          marginHorizontal: 5,
+          borderRadius: 10,
+          overflow: 'hidden',
+          backgroundColor: 'mediumseagreen',
+          justifyContent: 'space-evenly',
+          alignItems: 'center',
+          }}
+            >
+              <Ionicons name = "ios-book-outline" size = { 40 } color = { "white" }/>
+              <Text style = {{
+                              fontSize: FONT_SIZES.buttonTitle,
+                              color: "white",
+                              padding: 5,
+                            }}
+              >
+                Studies
+              </Text>
+            </Pressable>
+            <Pressable
+            style = {{
+          flex: 1, 
+          height: thisWindow.width / 2, 
+          marginHorizontal: 5,
+          borderRadius: 10,
+          overflow: 'hidden',
+          backgroundColor: 'mediumseagreen',
+          justifyContent: 'space-evenly',
+          alignItems: 'center',
+          }}
+            >
+              <Ionicons name = "ios-grid-outline" size = { 40 } color = { "white" }/>
+              <Text style = {{
+                              fontSize: FONT_SIZES.buttonTitle,
+                              color: "white",
+                              padding: 5,
+                            }}
+              >
+                Prayer Board
+              </Text>
+            </Pressable>
+          </View>
+        </View>
+      </ScrollView>    
+    </SafeAreaView>
   );
 };
 
-const RegGroupScreen = () => {
+const RegGroupScreen = ({ groupId }) => {
   return(
     <Text> I am not a member of this group </Text>
   );
 };
 
-const GroupScreen = ({ isPrivate, isMember, pic, title, nav }) => {
-  if (isMember) {
-    return <MemberGroup />;
+const GroupScreen = ({ navigation, route }) => {
+  if (route.params.isMember) {
+    return <MemberGroupScreen groupId = { route.params.groupId } />;
   }
-  return <RegGroup />;
+  return <RegGroupScreen groupId = { route.params.groupId } />;
 };
 
 /**
@@ -196,6 +334,13 @@ export const GroupsStackScreen = () => {
           },
         })}
       />
+      <GroupsStack.Screen
+        name = "Group"
+        component = { GroupScreen }
+        options = {({ route }) => ({ 
+          title: route.params.name, 
+        })}
+      />
     </GroupsStack.Navigator>
   );
 };
@@ -220,5 +365,19 @@ const styles = StyleSheet.create({
     resizeMode: 'cover',
     justifyContent: 'flex-end',
     alignItems: 'center',
+  },
+  tinyIcon: {
+    marginRight: 20,
+  },
+  pageHeader: {
+    flex: 1, 
+    flexDirection: 'row', 
+    justifyContent: 'flex-end',
+    alignItems: 'flex-end',
+    marginBottom: 10,
+  },
+  sectionHeader: {
+    fontSize: FONT_SIZES.sectionHeader, 
+    margin: 10,
   },
 });
